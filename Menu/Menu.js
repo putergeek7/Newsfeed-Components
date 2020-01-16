602c63d144ed5553f2fefdbf85feade81b502a17
Pat D <49631522+putergeek7@users.noreply.github.com>
2020-01-16 05:38:22 -0800

    completed - mvp

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+	'Students',
+	'Faculty',
+	"What's New",
+	'Tech Trends',
+	'Music',
+	'Log Out'
 ];
 
 /* 
@@ -33,3 +33,76 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+// define new elements
+function createMenu(menuItems) {
+	const menuList = document.createElement('div');
+	const ulList = document.createElement('ul');
+
+    // set text content, define new element, append
+    // setup structure of elements
+	menuItems.forEach(item => {
+		let listItem = document.createElement('li');
+		listItem.textContent = item;
+		ulList.appendChild(listItem);
+	});
+
+  // setup structure of elements
+	menuList.appendChild(ulList);
+// set class names
+	menuList.classList.add('menu');
+
+	// click event, toggles class menu open, click event to open menu
+	const menuButton = document.querySelector('.menu-button');
+	menuButton.addEventListener('click', () => {
+		menuList.classList.toggle('menu--open');
+  });
+  
+	return menuList;
+}
+ // setup structure of elements
+const header = document.querySelector('.header');
+menuItems.forEach(item => {
+	header.appendChild(createMenu(menuItems));
+});
+
+
+	// function showHiddenNav() {
+	// 	const menuButton = document.getElementById('menu--open');
+	// 	if (menuButton.className == 'menu-open') {
+	// 		menuButton.setAttribute('class', 'menu');
+	// 	} else {
+	// 		menuButton.setAttribute('class', 'menu--open');
+	// 	}
+	// }
+
+	// document.getElementById('menuButton').addEventListener('click', showHiddenNav);
+
+// function createArticles(title, date, firstParagraph, secondParagraph,
+// 	thirdParagraph) {
+// 	// define new elements
+// 	const article = document.createElement('div');
+// 	const articleTitle = document.createElement('h2');
+// 	const articleDate = document.createElement('p');
+// 	const articlePar1 = document.createElement('p');
+// 	const articlePar2 = document.createElement('p');
+// 	const articlePar3 = document.createElement('p');
+// 	const articleExpBtn = document.createElement('span');
+
+// article.appendChild(articleTitle);
+// 	article.appendChild(articleDate);
+// 	article.appendChild(articlePar1);
+// 	article.appendChild(articlePar2);
+// 	article.appendChild(articlePar3);
+// 	article.appendChild(articleExpBtn);
+
+// const menuBtn = document.querySelector('.menu-button');
+// let menuOpen = false;
+// menuBtn.addEventListener('click', () => {
+//   if(!menuOpen) {
+//   menuBtn.classList.add('open');
+//   menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove('open');
+//     menuOpen = false;
+//   }
+// } );
